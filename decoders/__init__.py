97ae5314fd4b68ea267710abd6ea7b82bf900b4f
Jeffrey Aylesworth <jeffrey@aylesworth.ca>
2010-01-03 16:19:44 -0500

    Function to decode any file (passes off to appropriate handler)

--- decoders/__init__.py
+++ decoders/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 """Handles decoders"""
 import config
+import os
 
 #Decoders
 import wave
@@ -8,3 +9,12 @@ handlers = { }
 for m in [wave]:
     for type in m.HANDLES:
         handlers[type] = m
+
+def decode(inF, outF):
+    """Decodes file inF to probably outF. outF is a hint as to where to decode
+    to, but might not. Returns the filename that the file was decoded to."""
+    type = os.path.splitext(inF)[1][1:].lower()
+    if type in handlers:
+        return handlers[type].decode(inF, outF)
+    else:
+        return False
