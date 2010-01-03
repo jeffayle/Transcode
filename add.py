e83676e2cfa6bfdef516640b60dbff483fd77bcd
Jeffrey Aylesworth <jeffrey@aylesworth.ca>
2010-01-03 01:01:45 -0500

    Checks if file exists, recurses into directories when adding

--- add.py
+++ add.py
@@ -15,6 +15,12 @@ def do():
 def addFile(buff, fname):
     "Adds a file to be processed"
     fname = os.path.realpath(fname) #Expand path
-    ##TODO: Add directories recursively
-    ##TODO: Check if file exists before adding
-    buff.write("%s\n"%(fname))
+    if os.path.isfile(fname):
+        buff.write("%s\n"%(fname))
+        print "Added '%s'"%(fname)
+    elif os.path.isdir(fname):
+        for fn in filter(lambda s: s[0]!='.', os.listdir(fname)):
+            #Recurse into non dot files/directories
+            addFile(buff, fn)
+    else:
+        sys.stderr.write("File not found '%s'\n"%(fname))
