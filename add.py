f5e4553c48965d4e797e7d844d131e34aa6d029a
Jeffrey Aylesworth <jeffrey@aylesworth.ca>
2010-01-02 22:05:34 -0500

    add command

--- add.py
+++ add.py
@@ -2,6 +2,17 @@
 """Adds files to be processed.
 Usage: add [file1 [file2 [file3 [... filen]]]]"""
 CMD="add"
+import config
+import os
+import sys
 
 def do():
-    print "Adding..."
+    f = open(os.path.join(config.STATE,"unprocessed"), "a")
+    for fn in sys.argv[2:]:
+        addFile(f, fn)
+
+def addFile(buff, fname):
+    "Adds a file to be processed"
+    ##TODO: Add directories recursively
+    ##TODO: Check if file exists before adding
+    buff.write("%s\n"%(fname))
