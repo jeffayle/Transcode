2bd82e142b1fa87df15e91a52b935337cfc35b7a
Jeffrey Aylesworth <jeffrey@aylesworth.ca>
2010-01-03 01:05:42 -0500

    Function to map a list onto dictionary

--- config.py
+++ config.py
@@ -12,3 +12,11 @@ def createState():
 def cmdName(cmd):
     "Returns the name of the command of a module"
     return cmd.CMD
+
+#Thanks to Mark Byers
+#http://stackoverflow.com/questions/1993840/map-list-onto-dictionary/1993853
+def mapDict(keyFunction, values):
+    "Maps a list onto a dictionary. Return value of keyFunction is used as \
+    the key, value is the original value"
+    return dict((keyFunction(v), v) for v in values)
+
