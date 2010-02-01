85a740ee634a5e199f4c3025feee744f38906b36
Jeffrey Aylesworth <jeffrey@aylesworth.ca>
2010-02-01 15:55:54 -0500

    command line interface

--- transcode.py
+++ transcode.py
@@ -1,3 +1,20 @@
 #!/usr/bin/env python
 import config
 import sys
+from doTranscode import transcode
+
+if len(sys.argv) >= 3:
+    inF = sys.argv[1]
+    outF = sys.argv[-1]
+    options = sys.argv[2:-1]
+    
+    succ = transcode(inF, outF, options)
+    if not succ:
+        sys.stderr.write("Something bad happened :(")
+        sys.stderr.write("Check encoder options\n")
+
+else:
+    print "Transcode: ImageMagick inspired audio transcoding"
+    print "---"
+    print "Usage:"
+    print "%s <input> [options] <output>"%(sys.argv[0])
