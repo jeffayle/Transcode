1590dbfdb8e3ffd6136b5e93ea36d3ee02ba03c6
Jeffrey Aylesworth <jeffrey@aylesworth.ca>
2010-02-01 12:52:53 -0500

    Encoders handler

---
+++ encoders/__init__.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+"""Handles encoders"""
+import config
+import os
+from glob import glob
+import sys
+import imp
+
+#Decoders
+files = glob("decoders/*.py")
+files.remove("decoders/__init__.py")
+modules = map(lambda m: imp.load_source(
+        os.path.basename(os.path.splitext(m)[0]), m), files)
+
+handlers = { }
+for m in modules:
+    for type in m.HANDLES:
+        handlers[type] = m
+
+def encode(inF, outF, type, options, metadata):
+    """Encodes inF to outF with given options and metadata"""
+    if type in handlers:
+        return handlers[type].encode(inF, outF, options, metadata)
+    else:
+        return None
