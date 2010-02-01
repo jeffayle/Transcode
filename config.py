b762f465c68a949829f35102628b6c686d578a6a
Jeffrey Aylesworth <jeffrey@aylesworth.ca>
2010-02-01 12:45:22 -0500

    Scrapped a lot, going to be a lot simpler

--- config.py
+++ config.py
@@ -3,13 +3,6 @@
 import os
 import subprocess
 
-STATE=os.path.expanduser("~/.transcode")
-
-def createState():
-    "Makes sure the state directory exists"
-    if not os.path.isdir(STATE):
-        os.mkdir(STATE)
-
 def cmdName(cmd):
     "Returns the name of the command of a module"
     return cmd.CMD
--- transcode.py
+++ transcode.py
@@ -1,33 +1,3 @@
 #!/usr/bin/env python
 import config
 import sys
-
-#Commands
-import add
-import list
-import clear
-import filter
-commands = config.mapDict(config.cmdName, [
-    add,
-    list,
-    clear,
-    filter
-])
-
-config.createState()
-
-#Check arguments
-if len(sys.argv) >= 2:
-    command = sys.argv[1]
-else:
-    command = None
-
-#Do that command
-if command:
-    if command in commands:
-        #do() that command
-        commands[command].do()
-    else:
-        sys.stderr.write("Unknown command '%s'\n"%(command))
-else:
-    sys.stderr.write("Usage: %s <command> [arguments]\n"%(sys.argv[0]))
