237dba3921d8c640b2f60d51b4122e2aa64a2277
Jeffrey Aylesworth <jeffrey@aylesworth.ca>
2010-02-01 13:25:22 -0500

    Build up command line options (flac)

--- encoders/flac.py
+++ encoders/flac.py
@@ -7,4 +7,22 @@ HANDLES=['flac','fla']
 DEFTYPE='flac'
 
 def encode(inF, outF, options, metadata):
-    print "Encoding to flac..."
+    cli_options = [ ]
+    cli_options.append("--output-name=%s"%outF) #Output file
+    #Metadata
+    if 'Title' in metadata:
+        cli_options.append("--tag=title=%s"%metadata['Title'])
+    if 'Artist' in metadata:
+        cli_options.append("--tag=artist=%s"%metadata['Artist'])
+    if 'Album' in metadata:
+        cli_options.append("--tag=album=%s"%metadata['Album'])
+    if 'Genre' in metadata:
+        cli_options.append("--tag=genre=%s"%metadata['Genre'])
+    if 'Track Number' in metadata:
+        cli_options.append("--tag=tracknumber=%s"%metadata['Track Number'])
+    if 'Date' in metadata:
+        cli_options.append("--tag=date=%s"%metadata['Date'])
+    #Add user's options
+    cli_options += options
+
+    return cli_options
