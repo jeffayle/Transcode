00cb04b4036b3c18ef43c33c38bb6a3356392e06
Jeffrey Aylesworth <jeffrey@aylesworth.ca>
2010-02-01 16:37:44 -0500

    Control quiet output from environment variable QUIET_TRANSCODE

--- transcode.py
+++ transcode.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 import config
 import sys
+import os
 from doTranscode import transcode
+
+if 'QUIET_TRANSCODE' in os.environ and os.environ['QUIET_TRANSCODE']!='0':
+    config.quiet = True
 
 if len(sys.argv) >= 3:
     inF = sys.argv[1]
