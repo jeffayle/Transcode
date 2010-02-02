6af550732ef31aa2f262733fb07142270830fab8
Jeffrey Aylesworth <jeffrey@aylesworth.ca>
2010-02-02 13:19:13 -0500

    mp3 decoder

---
+++ decoders/mp3.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+"""Handles decoding mp3 files"""
+import subprocess
+import config
+
+HANDLES=['mp3']
+
+def decode(inF, outF):
+    command = ['lame', '--decode']
+    if config.quiet:
+        command.append("--silent")
+    command += [inF, outF]
+    st = subprocess.call(command)
+
+    if st == 0:
+        return outF
+    else:
+        return False
+
+getMetadata = config.readExiftoolMetadata
