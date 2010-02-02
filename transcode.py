30acd941c374a90ba3e38d4c97f9712704e5c773
Jeffrey Aylesworth <jeffrey@aylesworth.ca>
2010-02-01 22:45:22 -0500

    Same name with different extension if only extension is given for output

--- transcode.py
+++ transcode.py
@@ -11,6 +11,11 @@ if len(sys.argv) >= 3:
     inF = sys.argv[1]
     outF = sys.argv[-1]
     options = sys.argv[2:-1]
+
+    #If the output is just an extension (first character is a dot) use the same
+    #name as the input file, with new extension
+    if outF[0] == '.':
+        outF = os.path.splitext(inF)[0] + outF
     
     succ = transcode(inF, outF, options)
     if not succ:
