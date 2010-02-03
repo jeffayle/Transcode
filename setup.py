8aa5abc0f9962ecf1c2efb1f31163cd0ecc31bcf
Jeffrey Aylesworth <jeffrey@aylesworth.ca>
2010-02-02 23:00:37 -0500

    distutils setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name = 'AudioTranscode',
+    version = '1.0',
+    packages = ['audioTranscode'],
+    scripts = ['transcode'],
+    author = 'Jeffrey Aylesworth',
+    author_email = 'jeffrey@aylesworth.ca',
+    license = 'MIT',
+    url = 'http://github.com/jeffayle/Transcode'
+)
