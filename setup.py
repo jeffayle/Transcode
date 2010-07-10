d0d4f5220ad1e801327fc39a31591a55cb46e5b7
Jeffrey Aylesworth <jeffrey@aylesworth.ca>
2010-07-10 14:53:51 -0400

    Version 2

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from distutils.core import setup
 
 setup(
     name = 'AudioTranscode',
-    version = '1.1',
+    version = '2.0',
     packages = ['audioTranscode','audioTranscode.encoders','audioTranscode.decoders'],
-    scripts = ['transcode'],
+    scripts = ['transcode', 'transcodeall'],
     author = 'Jeffrey Aylesworth',
     author_email = 'jeffrey@aylesworth.ca',
     license = 'MIT',
